4ca657efb5308c55f669b93f1688a8fdbc770f82
Miguel Castillo <programador.id02@comunidadincap.edu.co>
2026-03-16 03:38:31 -0500

    fix(formatCurrency): remove decimal digits for currency display

--- src/utils/formatCurrency.ts
+++ src/utils/formatCurrency.ts
@@ -2,5 +2,6 @@ export const formatCurrency = (amount: number, currency: string) => {
   return new Intl.NumberFormat(undefined, {
     style: "currency",
     currency,
+    maximumFractionDigits: 0,
   }).format(amount);
 };
