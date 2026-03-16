516e001dcf804d849efef10d27d66a9dbf588069
Miguel Castillo <programador.id02@comunidadincap.edu.co>
2026-03-16 16:30:02 -0500

    refactor: disable some navbar features

--- src/features/navbar/components/navbar.config.ts
+++ src/features/navbar/components/navbar.config.ts
@@ -20,16 +20,16 @@ export type NavItemsConfig = Record<string, NavItemConfig[]>;
 export const navItemsConfig: NavItemsConfig = {
   public: [{ to: "/pay", icon: CircleDollarSign, label: "Pay here" }],
   admin: [
-    { to: "/dashboard", icon: LayoutDashboard, label: "Dashboard" },
+    // { to: "/dashboard", icon: LayoutDashboard, label: "Dashboard" },
     { to: "/invoices", icon: FileText, label: "Invoices" },
     { to: "/transactions", icon: ArrowLeftRight, label: "Transactions" },
-    { to: "/api-keys", icon: KeyRound, label: "API Keys" },
+    // { to: "/api-keys", icon: KeyRound, label: "API Keys" },
   ],
   maintainer: [
     { to: "/dashboard", icon: LayoutDashboard, label: "Dashboard" },
-    { to: "/commerces", icon: Building2, label: "Commerces" },
-    { to: "/administrators", icon: Users, label: "Administrators" },
+    // { to: "/commerces", icon: Building2, label: "Commerces" },
+    // { to: "/administrators", icon: Users, label: "Administrators" },
     { to: "/transactions", icon: ArrowLeftRight, label: "Transactions" },
-    { to: "/api-keys", icon: KeyRound, label: "API Keys" },
+    // { to: "/api-keys", icon: KeyRound, label: "API Keys" },
   ],
 };
